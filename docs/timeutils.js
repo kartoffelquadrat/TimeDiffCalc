75ea2f6fae66b069d06381e034f28ff5224c4c03
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-09-24 10:42:26 -0400

    enabling buttons and input reactions

--- docs/timeutils.js
+++ docs/timeutils.js
@@ -1,15 +1,25 @@
 function registerElementListeners() {
-    document.getElementById('start-time-hours-field').addEventListener("keypress", () => updateDiffField());
-    document.getElementById('start-time-minutes-field').addEventListener("keypress", () => updateDiffField());
-    document.getElementById('end-time-hours-field').addEventListener("keypress", () => updateDiffField());
-    document.getElementById('end-time-minutes-field').addEventListener("keypress", () => updateDiffField());
 
+    // Update on any keystroke in input field (no return press required)
+    document.getElementById('start-time-hours-field').addEventListener("keypress", function(e) {updateDiffField(e)});
+    document.getElementById('start-time-minutes-field').addEventListener("keypress", function(e) {updateDiffField(e)});
+    document.getElementById('end-time-hours-field').addEventListener("keypress", function(e) {updateDiffField(e)});
+    document.getElementById('end-time-minutes-field').addEventListener("keypress", function(e) {updateDiffField(e)});
+    document.getElementById('end-time-minutes-field').addEventListener("keypress", function(e) {updateDiffField(e)});
+
+    // Register cache button
+    document.getElementById("cache-button").addEventListener("click", () => addToHistory())
 }
 
-function updateDiffField() {
-    alert("A key was typed")
+function updateDiffField(e) {
+    if (e.key === "Escape") {
+        alert("esc pressed!");
+    } else {
+        alert("A key was typed");
+    }
 }
+
 
 function addToHistory() {
-
+    alert("Extending history");
 }
